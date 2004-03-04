37ca3a4f60af383e9ca40a6f167c82a63a7710e3
Damon Chaplin <damon@gnome.org>
2004-03-04 17:34:52 +0000

    fixed URLs to download autoconf/automake/libtool. Patch from Frederic L.
    2004-03-04  Damon Chaplin  <damon@gnome.org>
    
    	* autogen.sh: fixed URLs to download autoconf/automake/libtool.
    	Patch from Frederic L. W. Meunier.

--- autogen.sh
+++ autogen.sh
@@ -18,15 +18,15 @@ cd "$srcdir"
 	echo
 	echo "You must have autoconf installed to compile $PROJECT."
 	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	echo "or get the source tarball at ftp://ftp.gnu.org/gnu/autoconf/"
 	DIE=1
 }
 
 (libtool --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have libtool installed to compile $PROJECT."
-	echo "Get ftp://alpha.gnu.org/gnu/libtool-1.2b.tar.gz"
-	echo "(or a newer version if it is available)"
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/gnu/libtool/"
 	DIE=1
 }
 
@@ -42,8 +42,8 @@ elif automake-1.4 --version < /dev/null > /dev/null 2>&1; then
 else
 	echo
 	echo "You must have automake installed to compile $PROJECT."
-	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
-	echo "(or a newer version if it is available)"
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/gnu/automake/"
 	DIE=1
 fi
 
