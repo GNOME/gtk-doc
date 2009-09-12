de5d8c37776f150b0e2b807f937d0d58cb529a4e
Stefan Kost <ensonic@users.sf.net>
2009-09-12 14:39:17 +0300

    annotations: allow annotations in struct/union/enum members
    Refactor the code that expands annotations. Also add a test for structs.

--- tests/annotations/src/tester.h
+++ tests/annotations/src/tester.h
@@ -4,6 +4,16 @@
 #include <glib.h>
 #include <glib-object.h>
 
+/**
+ * GtkdocAnnotation:
+ * @that: (allow-none): eventualy points to something
+ *
+ * small struct
+ */
+struct _GtkdocAnnotation {
+  gpointer that;
+};
+
 extern void annotation_array_length (GObject *list, gint n_columns, GType *types);
 
 extern gchar * annotation_nullable (const gchar *uri, const gchar *label);
