GNOME/gtk-doc

commit d5110ef6d1825fc1f4f7f7bcfe149e48ede972f8

make: remove the types file on clean if we generated it

gtk-doc.make
@@ -242,6 +242,9 @@ pdf-build.stamp: sgml.stamp $(DOC_MAIN_SGML_FILE) $(content_files)
 clean-local:
 	@rm -f *~ *.bak
 	@rm -rf .libs
+	@if echo $(SCAN_OPTIONS) | grep -q "\-\-rebuild-types" ; then \
+	  rm $(DOC_MODULE).types; \
+	fi
 
 distclean-local:
 	@rm -rf xml html $(REPORT_FILES) $(DOC_MODULE).pdf \

gtk-doc.notmpl.make
@@ -219,6 +219,9 @@ pdf-build.stamp: sgml.stamp $(DOC_MAIN_SGML_FILE) $(content_files)
 clean-local:
 	@rm -f *~ *.bak
 	@rm -rf .libs
+	@if echo $(SCAN_OPTIONS) | grep -q "\-\-rebuild-types" ; then \
+	  rm $(DOC_MODULE).types; \
+	fi
 
 distclean-local:
 	@rm -rf xml html $(REPORT_FILES) $(DOC_MODULE).pdf \

tests/gtk-doc.make
@@ -214,6 +214,9 @@ pdf-build.stamp: sgml.stamp $(DOC_MAIN_SGML_FILE) $(content_files)
 clean-local:
 	@rm -f *~ *.bak ts
 	@rm -rf .libs
+	@if echo $(SCAN_OPTIONS) | grep -q "\-\-rebuild-types" ; then \
+	  rm $(DOC_MODULE).types; \
+	fi
 	$(MAKE) distclean-local
 
 distclean-local:

tests/gtk-doc.notmpl.make
@@ -190,6 +190,9 @@ pdf-build.stamp: sgml.stamp $(DOC_MAIN_SGML_FILE) $(content_files)
 clean-local:
 	@rm -f *~ *.bak ts
 	@rm -rf .libs
+	@if echo $(SCAN_OPTIONS) | grep -q "\-\-rebuild-types" ; then \
+	  rm $(DOC_MODULE).types; \
+	fi
 	$(MAKE) distclean-local
 
 distclean-local:

commit 73e5c7c5733b1754a43c2dc38c189fc51a22350a

make: Don't fail on removing types files if they don't exist
This fixes make dist, broken by d5110ef6d1825fc1f4f7f7bcfe149e48ede972f8

gtk-doc.make
@@ -243,7 +243,7 @@ clean-local:
 	@rm -f *~ *.bak
 	@rm -rf .libs
 	@if echo $(SCAN_OPTIONS) | grep -q "\-\-rebuild-types" ; then \
-	  rm $(DOC_MODULE).types; \
+	  rm -f $(DOC_MODULE).types; \
 	fi
 
 distclean-local:

gtk-doc.notmpl.make
@@ -220,7 +220,7 @@ clean-local:
 	@rm -f *~ *.bak
 	@rm -rf .libs
 	@if echo $(SCAN_OPTIONS) | grep -q "\-\-rebuild-types" ; then \
-	  rm $(DOC_MODULE).types; \
+	  rm -f $(DOC_MODULE).types; \
 	fi
 
 distclean-local:

tests/gtk-doc.make
@@ -215,7 +215,7 @@ clean-local:
 	@rm -f *~ *.bak ts
 	@rm -rf .libs
 	@if echo $(SCAN_OPTIONS) | grep -q "\-\-rebuild-types" ; then \
-	  rm $(DOC_MODULE).types; \
+	  rm -f $(DOC_MODULE).types; \
 	fi
 	$(MAKE) distclean-local
 

tests/gtk-doc.notmpl.make
@@ -191,7 +191,7 @@ clean-local:
 	@rm -f *~ *.bak ts
 	@rm -rf .libs
 	@if echo $(SCAN_OPTIONS) | grep -q "\-\-rebuild-types" ; then \
-	  rm $(DOC_MODULE).types; \
+	  rm -f $(DOC_MODULE).types; \
 	fi
 	$(MAKE) distclean-local
 