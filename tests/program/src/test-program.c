a9864c47fdd8abfd52978f100d654d6880908d66
Thomas Wood <thomas.wood@intel.com>
2015-07-08 14:27:46 +0100

    Add support for inline program documentation
    Create program documentation from documentation comments in source
    files.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=752126

---
+++ tests/program/src/test-program.c
@@ -0,0 +1,19 @@
+
+/**
+ * PROGRAM:test-program
+ * @short_description: A test program
+ * @synopsis: test-program [*OPTIONS*...] --arg1 *arg* *FILE*
+ * @see_also: test(1)
+ * @--arg1 *arg*: set arg1 to *arg*
+ * @-v, --version: Print the version number
+ * @-h, --help: Print the help message
+ *
+ * Long description of program.
+ *
+ * Returns: Zero on success, non-zero on failure
+ */
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
