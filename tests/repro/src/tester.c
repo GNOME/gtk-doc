a3a22ca1b6154c5a453b2cb75547469944936ca6
Stefan Sauer <ensonic@users.sf.net>
2017-11-04 11:45:57 +0100

    tests: add a minimal test suite we can use for repro cases

---
+++ tests/repro/src/tester.c
@@ -0,0 +1,8 @@
+/**
+ * SECTION:tester
+ * @short_description: module for gtk-doc unit test
+ *
+ * This is a minimal doc module to serve easy to debug repro cases.
+ */
+
+#include "tester.h"
---
+++ tests/repro/src/tester.h
@@ -0,0 +1,14 @@
+#ifndef GTKDOC_TESTER_H
+#define GTKDOC_TESTER_H
+
+#include <glib.h>
+
+// FIXME: this should not be needed, if we have a title + long-desc
+/**
+ * FOO:
+ *
+ * Placeholder to have non empty docs.
+ */
+#define FOO 1
+
+#endif // GTKDOC_TESTER_H
