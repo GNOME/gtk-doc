bdd4b1b3d4287fa4bfa997c93342213d8220ce7b
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-09-12 22:01:07 +0200

    scangobj: keep quotes when splitting with shlex
    shlex.split doesn't keep double quotes when splitting, thus we need to redefine
    it so that double quotes are considered quotes by the lexer.
    
    Use it to parse ldflags and cflags.
    
    Added test-gobject-scangobj-quoted-cflags to verify the new behavior.
    
    Fixes GNOME/gtk-doc#57

--- tests/helpers/gtkdoc_scangobj_runner.py
+++ tests/helpers/gtkdoc_scangobj_runner.py
@@ -17,6 +17,8 @@ if __name__ == '__main__':
                         help='Path to the pkg-config executable to be used')
     parser.add_argument("--extra-pkg", type=str, default=[], action='append',
                         help='Extra package to be use while scanning')
+    parser.add_argument("--extra-cflags", type=str, default=[], action='append',
+                        help='Extra Cflags to be use while scanning')
     parser.add_argument("--extra-lib", type=str, default=[], action='append',
                         help='Extra library to be use while scanning')
 
@@ -33,6 +35,7 @@ if __name__ == '__main__':
     if process.returncode == 0:
         cflags += output.rstrip().decode('utf-8').split(' ')
 
+    cflags += options.extra_cflags
     arguments.append('--cflags={0}'.format(' '.join(cflags)))
 
     process = Popen([options.pkg_config,
