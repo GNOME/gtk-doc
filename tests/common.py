f13318f30370cbcd8ad2fe662572f553fac60152
Stefan Sauer <ensonic@users.sf.net>
2018-12-12 21:06:36 +0100

    tests: fix deprecation warning and make text executable

--- tests/common.py
+++ tests/common.py
@@ -49,19 +49,19 @@ class TestGetModuleDocDir(unittest.TestCase):
     @mock.patch('subprocess.check_output')
     def test_ReturnsPath(self, subprocess_check_output):
         subprocess_check_output.return_value = '/usr'
-        self.assertEquals(common.GetModuleDocDir('glib-2.0'), '/usr/share/gtk-doc/html')
+        self.assertEqual(common.GetModuleDocDir('glib-2.0'), '/usr/share/gtk-doc/html')
 
 
 class TestCreateValidSGMLID(unittest.TestCase):
 
     def test_AlreadyValid(self):
-        self.assertEquals(common.CreateValidSGMLID('x'), 'x')
+        self.assertEqual(common.CreateValidSGMLID('x'), 'x')
 
     def test_SpecialCharsBecomeDash(self):
-        self.assertEquals(common.CreateValidSGMLID('x_ y'), 'x--y')
+        self.assertEqual(common.CreateValidSGMLID('x_ y'), 'x--y')
 
     def test_SpecialCharsGetRemoved(self):
-        self.assertEquals(common.CreateValidSGMLID('x,;y'), 'xy')
+        self.assertEqual(common.CreateValidSGMLID('x,;y'), 'xy')
 
 
 if __name__ == '__main__':
